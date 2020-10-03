27b107eedb918591aaa99bf5f8ae2cfab5517700
yaguchi <yaguchi@example.com>
2020-10-03 18:46:44 +0900

    1st commit

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+// $_POST の値をJSONにして返す簡単なお仕事
+header('Content-Type: application/json; charset=utf-8');
+//echo json_encode(['text' => $_POST['text']]);
+echo json_encode(['text' => "postofapi"]);
